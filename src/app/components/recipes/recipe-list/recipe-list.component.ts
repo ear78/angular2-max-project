7ed696517f84b78f8a1d734f17d1ad5772913737
Elliot Richardson <ear78@aol.com>
2017-07-11 00:10:19 -0500

    created ingredient model and setup shopping list component and wired up data

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is a description for testing', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrEOhGKKoj_JHTNwCZLQHx42AS-mvCvWo0FINn5IjkLT2sjHvp')
+    new Recipe('A test recipe', 'this is a description for testing', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrEOhGKKoj_JHTNwCZLQHx42AS-mvCvWo0FINn5IjkLT2sjHvp'),
+    new Recipe('A Another recipe', 'this is a description for test 2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS72WN0zSQ9mx51ea_2YZHC_2o3wGrqiqunefokoYind-ewxsODYg')
   ];
 
   constructor() { }
